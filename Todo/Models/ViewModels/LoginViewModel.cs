d6cce01a51f8e9bacaecc5029245f2d0a249b1bd
a-c-everett <andreweverett16@gmail.com>
2024-06-20 12:48:25 -0400

    Added ViewModel for logging in

---
+++ Todo/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username {get; set;}
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password {get; set;}  
+          
+    }
+}
--- Todo/obj/Debug/net8.0/Todo.AssemblyInfo.cs
+++ Todo/obj/Debug/net8.0/Todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c2d97bfe8cfc910d2f85ad6663d6e05808e697")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ea7561420d119219e6f970891fbbea11c61f47")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
